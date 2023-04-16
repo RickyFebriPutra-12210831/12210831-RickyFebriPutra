d697481025c8e3380bef5fef82cfd3486c634213
RickyFebriPutra-12210831 <putraricky244@gmail.com>
2023-04-16 16:21:34 +0700

    tugas lanjutan 4 dan 5

---
+++ lib/views/login_view.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:pendataanwarga/views/login_view.dart';
+import 'package:pendataanwarga/views/dashboard_view.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 90, bottom: 60),
+            child: Text(
+              'Aplikasi\nPendataan Warga',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Stack(
+            children: [
+              Container(
+                height: 380,
+                child: Card(
+                  margin: EdgeInsets.only(top: 50, left: 20, right: 20),
+                  color: Colors.brown[200],
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 100,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              fillColor: Colors.grey[50],
+                              filled: true),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              fillColor: Colors.grey[50],
+                              filled: true),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (bc) {
+                            return Dashboardview();
+                          }));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: Text('Login'),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            primary: Colors.red),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Center(
+                  child: Image.asset(
+                'assets/Logo.png',
+                width: 150,
+              )),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
